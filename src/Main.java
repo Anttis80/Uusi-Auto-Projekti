c740724767f1012366fb394e63aef2b6b7c45220
Anttis80 <90746169+Anttis80@users.noreply.github.com>
2021-12-06 16:15:10 +0200

    auto1 olio luotu

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		
+		Auto auto1 = new Auto("Toyota", "Corolla", 10);
+		auto1.naytaTiedot();
+		auto1.kiihdyta();
+		auto1.naytaTiedot();	
 	}
 
 }
@@ -13,6 +16,21 @@ class Auto
 	public String merkki;
 	public String malli;
 	public int bensanMaara;
+	
+	public Auto()
+	{
+		merkki = "";
+		malli = "";
+		bensanMaara = 0;
+	}
+	
+	public Auto(String merkki, String malli, int bensa)
+	{
+		this.merkki = merkki;
+		this.malli = malli;
+		bensanMaara = bensa;
+	}
+
 
 	public void jarruta()
 	{
@@ -29,10 +47,11 @@ class Auto
 			
 	}
 	
-	private void naytaTiedot()
+	public void naytaTiedot()
 	{
 		System.out.println("Merkki: " + merkki);
 		System.out.println("Malli: " + malli);
 		System.out.println("Bensan m‰‰r‰: " + bensanMaara);
+		System.out.println();
 	}
 }
